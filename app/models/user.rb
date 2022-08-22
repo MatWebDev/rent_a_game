6d76e08dc49232ea2f6e99a53217bc2441d0d759
mathieu ferry <mathieu.ferry87@gmail.com>
2022-08-22 16:01:11 +0200

    db create models create validatations

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  has_many :bookings, dependent: :destroy
+  has_many :games, dependent: :destroy
+
+  validates :first_name, :last_name, :email, presence: true
+end
---
+++ db/migrate/20220822131701_create_bookings.rb
@@ -0,0 +1,13 @@
+class CreateBookings < ActiveRecord::Migration[7.0]
+  def change
+    create_table :bookings do |t|
+      t.date :starting_date
+      t.date :ending_date
+      t.float :total_price
+      t.references :user, null: false, foreign_key: true
+      t.references :game, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
