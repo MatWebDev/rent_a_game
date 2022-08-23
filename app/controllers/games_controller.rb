efc4ce67000661a602cadb904a920685aba80984
jerome pomonti <jerome.pomonti@gmail.com>
2022-08-23 10:26:52 +0200

    controllergames

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,33 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+
+  def create
+    @game = Game.create!(game_params)
+    if @game.save
+      redirect_to root_path, notice: "Votre annonce à bien été créer."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    @game = game.find(params[:id])
+  end
+
+  def new
+    @game = game.new
+  end
+
+  def destroy
+    @game = game.find(params[:id])
+    @game.destroy
+    redirect_to games_path, status: :see_other
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit(:name, :photo)
+  end
 end
