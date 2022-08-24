851c29e1fd005ad4310482046edb12f7510dcdec
mathieu ferry <mathieu.ferry87@gmail.com>
2022-08-24 15:54:03 +0200

    gestion dispo de annonces

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_24_090057) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_24_134930) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -59,12 +59,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_24_090057) do
     t.text "description"
     t.string "address"
     t.float "price_per_day"
-    t.boolean "disponibility"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.boolean "disponibility", default: true
     t.index ["user_id"], name: "index_games_on_user_id"
   end
 
